c643339d788fff767c5db9316df83ca03f46df10
Sean Coetzee <sean.coetzee@fnb.co.za>
2019-11-01 13:03:01 +0200

    Tests

--- src/test/java/za/co/sceoan/phonebook/resources/PhoneBookResourceTest.java
+++ src/test/java/za/co/sceoan/phonebook/resources/PhoneBookResourceTest.java
@@ -3,16 +3,22 @@ package za.co.sceoan.phonebook.resources;
 import io.quarkus.test.junit.QuarkusTest;
 import static io.restassured.RestAssured.given;
 import java.util.UUID;
+import java.util.logging.Logger;
 import static org.hamcrest.CoreMatchers.containsString;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class PhoneBookResourceTest {
+    private static final Logger LOG = Logger.getLogger(PhoneBookResourceTest.class.getName());
+    
     private static final String EMAIL = UUID.randomUUID() + "@test.com";
     private static final String PASSWORD = UUID.randomUUID().toString();
     private static final String NAME = "TEST";
-    private static final String CONTACT = "{name: \"test\", email: \"test@test.com\", phone: \"0115555555\"}";
+    private static final String CONTACT = "{\"name\": \"test\", \"email\": \"test@test.com\", \"phone\": \"0115555555\"}";
     private static final String USER = "{\"email\":\"" + EMAIL + "\",\"password\":\"" + PASSWORD + "\",\"name\":\"" + NAME + "\"}";
 
     @BeforeEach
@@ -25,7 +31,7 @@ public class PhoneBookResourceTest {
     }
     
     @Test
-    public void addContact() {
+    public void postContact() {
         given()
                 .auth()
                 .preemptive()
@@ -38,15 +44,57 @@ public class PhoneBookResourceTest {
     }
     
     @Test
-    public void testSearch() {
+    public void search() {
         given()
                 .auth()
                 .preemptive()
                 .basic(EMAIL, PASSWORD)
-                .when().get("/phonebook/api/v1/phonebook/search?s=TEST")
+                .when().get("/phonebook/api/v1/phonebook/search?s=test")
                 .then()
                 .statusCode(200)
                 .assertThat()
-                .body(containsString("TEST"));
+                .body(containsString("\"test\""));
+    }
+    
+    @Test
+    public void getInitials() {
+        given()
+                .auth()
+                .preemptive()
+                .basic(EMAIL, PASSWORD)
+                .when().get("/phonebook/api/v1/phonebook/initial")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body(containsString("\"t\""));
+    }
+    
+    @Test
+    public void getByInitial() {
+        String id = given()
+                .auth()
+                .preemptive()
+                .basic(EMAIL, PASSWORD)
+                .when().get("/phonebook/api/v1/phonebook/initial/t")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body(containsString("\"test\""))
+                .extract()
+                .path("$.[0].id");
+        LOG.info(id);
+    }
+    
+    @Test
+    public void getAllContacts() {
+        given()
+                .auth()
+                .preemptive()
+                .basic(EMAIL, PASSWORD)
+                .when().get("/phonebook/api/v1/phonebook")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body(containsString("\"test\""));
     }
 }
